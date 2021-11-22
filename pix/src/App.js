b2b5001d1e4f48397095cf1deaaa40640375cf80
Kira <math.descamps5962@gmail.com>
2021-11-22 16:22:53 +0100

    P letter is okay (number of photos per height and per width).

--- pix/src/App.js
+++ pix/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './style/App.css';
 import Logo from './components/Logo';
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="React app">
       <div id="logo-wrapper">
         <Logo />
-    </div>
+      </div>
     </div>
   );
 }
