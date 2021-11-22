0590cb4a2a0d36c993f2c347d5cf389b8512b4dd
Kira <math.descamps5962@gmail.com>
2021-11-22 12:01:18 +0100

    Switched to a React app.

---
+++ pix/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Logo from './components/Logo';
+
+function App() {
+  return (
+    <div className="React app">
+      <div id="logo-wrapper">
+        <Logo />
+    </div>
+    </div>
+  );
+}
+
+export default App;
