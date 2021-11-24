f5a48495d9ad0934fafbc3e58c70fc59c98f1076
Kira <math.descamps5962@gmail.com>
2021-11-24 10:44:54 +0100

    Entire logo is now filled with images from public/photos folder + added manifest.json file.

--- pix/src/components/Logo.js
+++ pix/src/components/Logo.js
@@ -7,8 +7,6 @@ function importAll(r) {
    }
    const images = importAll(require.context('../../public/photos', false, /\.(png|jpe?g|svg)$/));
 
-   console.log('images here -> ', images);
-
    let photos = Object.keys(images);
    console.log('photos here ->', photos);
 
@@ -209,84 +207,138 @@ function Logo() {
         "https://picsum.photos/126/126?random=129"
     ]
 
-    let imgArray = xSquare.length;
-    console.log(imgArray);
-
-    
+    let photosIndex = 1;
 
     return (
         <>
             <div className="p-letter">
-                {/* 152 photos */}
+                {/* 152 photos max */}
                 <div className="round-part">
                     { 
                         photos.map((photo, index) => {
-                        console.log('the photo : ', photo);
-                        return <img src={'/photos/' + photo} key={index} />
-                    }) }
-                    {
-                        
+                            if(photosIndex < 153) {
+                                photosIndex++;
+                                return <img src={'/photos/' + photo} key={index} />
+                            }
+                            /* At the end photosIndex = 152 */
+                        }) 
                     }
                 </div>
+                {/* 42 photos */}
                 <div className="straight-part">
-                    {pStraightPart.map((img, index) => {
-                        return <img src={img} key={index} />
-                    })}
+                    { 
+                        photos.map((photo, index) => {
+                            if((index > photosIndex) && (index < 195)) {
+                                photosIndex++;
+                                return <img src={'/photos/' + photo} key={index} />
+                            }
+                             /* At the end photosIndex = 194 */
+                        }) 
+                    }
                 </div>
             </div>
             <div className="i-letter">
+                {/* 36 photos */}
                 <div className="point-part">
-                    {iPointPart.map((img, index) => {
-                        return <img src={img} key={index} />
-                    })}
+                    { 
+                        photos.map((photo, index) => {
+                            if((index > photosIndex) && (index < 231)) {
+                                photosIndex++;
+                                return <img src={'/photos/' + photo} key={index} />
+                            }
+                            /* At the end photosIndex = 230 */
+                        }) 
+                    }
                 </div>
+                {/* 60 photos */}
                 <div className="straight-part">
-                    {iStraightPart.map((img, index) => {
-                        return <img src={img} key={index} />
-                    })}
+                    { 
+                        photos.map((photo, index) => {
+                            if((index > photosIndex) && (index < 291)) {
+                                photosIndex++;
+                                return <img src={'/photos/' + photo} key={index} />
+                            }
+                            /* At the end photosIndex = 290 */
+                        }) 
+                    }
                 </div>
             </div>
             <div className="x-letter">
                 <div className="col col-1">
+                    {/* 36 photos */}
                     <div className="square">
-                        {xSquare.map((img, index) => {
-                            return <img src={img} key={index} />
-                        })}
+                        { 
+                            photos.map((photo, index) => {
+                                if((index > photosIndex) && (index < 327)) {
+                                    photosIndex++;
+                                    return <img src={'/photos/' + photo} key={index} />
+                                }
+                                /* At the end photosIndex = 326 */
+                            }) 
+                        }
                     </div>
                     <div className="square hidden">
                     </div>
+                    {/* 36 photos */}
                     <div className="square">
-                        {xSquare.map((img, index) => {
-                            return <img src={img} key={index} />
-                        })}
+                        { 
+                            photos.map((photo, index) => {
+                                if((index > photosIndex) && (index < 364)) {
+                                    photosIndex++;
+                                    return <img src={'/photos/' + photo} key={index} />
+                                }
+                                /* At the end photosIndex = 363 */
+                            }) 
+                        }
                     </div>
                 </div>
                 <div className="col col-2">
                     <div className="square hidden">
                         
                     </div>
+                    {/* 36 photos */}
                     <div className="square">
-                        {xSquare.map((img, index) => {
-                            return <img src={img} key={index} />
-                        })}
+                        { 
+                            photos.map((photo, index) => {
+                                if((index > photosIndex) && (index < 400)) {
+                                    photosIndex++;
+                                    return <img src={'/photos/' + photo} key={index} />
+                                }
+                                /* At the end photosIndex = 399 */
+                            }) 
+                        }
                     </div>
                     <div className="square hidden">
 
                     </div>
                 </div>
                 <div className="col col-3">
+                    {/* 36 photos */}
                     <div className="square">
-                        {xSquare.map((img, index) => {
-                            return <img src={img} key={index} />
-                        })}
+                        { 
+                            photos.map((photo, index) => {
+                                if((index > photosIndex) && (index < 436)) {
+                                    photosIndex++;
+                                    return <img src={'/photos/' + photo} key={index} />
+                                }
+                                /* At the end photosIndex = 435 */
+                            }) 
+                        }
                     </div>
                     <div className="square hidden">
 
                     </div>
+                    {/* 36 photos */}
                     <div className="square">
-                        {xSquare.map((img, index) => {
-                            return <img src={img} key={index} />
-                        })}
+                        { 
+                            photos.map((photo, index) => {
+                                if((index > photosIndex) && (index < 472)) {
+                                    photosIndex++;
+                                    return <img src={'/photos/' + photo} key={index} />
+                                }
+                                /* At the end photosIndex = 471 */
+                            }) 
+                        }
                     </div>
                 </div>
             </div>
