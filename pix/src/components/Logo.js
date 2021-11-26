d3ba3b0919d0e6007200086a0582eb33cdaadbc0
Louis-Etienne Leuilliot <louis-etienne@leuilliot.fr>
2021-11-26 18:05:18 +0100

    update lou

--- pix/src/components/Logo.js
+++ pix/src/components/Logo.js
@@ -1,119 +1,138 @@
-import React from 'react';
-
-function importAll(r) {
-    let images = {};
-     r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
-    return images
-   }
-   const images = importAll(require.context('../../public/photos', false, /\.(webp|jpe?g)$/));
-
-   let photos = Object.keys(images);
-   console.log('photos here ->', photos);
+import React, { useState }  from 'react';
+/*p = (9 * 8)  + (4*4) = 72 + 16 = 88*/
+/*I= 4*4 + 4*7 =  16 + 28 = 44*/
+/*X = (4*4 )*5 = 16 * 5 = 80 */
+/*Total = 212 photos */
 
 
-function Logo() {
-
-    let photosIndex = 1;
-
+function P(props){
     return (
         <>
             <div className="p-letter">
-                {/* 152 photos max */}
                 <div className="round-part">
-                    { 
-                        photos.map((photo, index) => {
-                            if(photo && photosIndex < 153) {
-                                photosIndex++;
-                                return <img src={'/photos/' + photo} key={index} />
-                            } else {
-                                return <img src="" />
-                            }
-                            /* At the end photosIndex = 152 */
-                        }) 
+                    {props.pRoundPart.length > 0 &&
+                        props.pRoundPart
                     }
                 </div>
-                {/* 42 photos */}
                 <div className="straight-part">
-                    { 
-                        photos.map((photo, index) => {
-                            if((index > photosIndex) && (index < 195)) {
-                                photosIndex++;
-                                return <img src={'/photos/' + photo} key={index} />
-                            }
-                             /* At the end photosIndex = 194 */
-                        }) 
+                    {props.pStraightPart.length > 0 &&
+                        props.pStraightPart
                     }
                 </div>
             </div>
+        </>
+    )
+}
+
+
+function Logo(){
+
+    function importAll(r) {
+        let images = {};
+        r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
+        return images
+    }
+
+    const images    = importAll(require.context('../../public/photos', false, /\.(webp|jpe?g)$/));
+    let photos      = Object.keys(images);
+
+
+    const pRoundPart = [];
+    const pStraightPart = [];
+    const iPointPart = [];
+    const iStraightPart = [];
+    const xSquare1 = [];
+    const xSquare2 = [];
+    const xSquare3 = [];
+    const xSquare4 = [];
+    const xSquare5 = [];
+
+    function fetchData(){
+         for (var i = 0; i < 72; i++) {
+            var elImg = <img className={'fadeIn fadeOut'} src={'/photos/' + photos[i]}  />;
+            pRoundPart.push(elImg);
+        }
+
+        for (var i = 72; i < 88; i++) {
+            var elImg = <img className={'fadeIn fadeOut'} src={'/photos/' + photos[i]}  />;
+            pStraightPart.push(elImg);
+        }
+
+        for (var i = 88 ; i < 104; i++ ){
+            var elImg = <img src={'/photos/' + photos[i]}  />;
+            iPointPart.push(elImg);
+        }
+
+        for (var i = 104 ; i < 132; i++ ){
+            var elImg = <img src={'/photos/' + photos[i]} />;
+            iStraightPart.push(elImg);
+        }
+
+        for (var i = 132 ; i < 148; i++ ){
+            var elImg = <img src={'/photos/' + photos[i]} key={i} />;
+            xSquare1.push(elImg);
+        }
+
+        for (var i = 148 ; i < 164; i++ ){
+            var elImg = <img src={'/photos/' + photos[i]} key={i} />;
+            xSquare2.push(elImg);
+        }
+
+        for (var i = 164 ; i < 180; i++ ){
+            var elImg = <img src={'/photos/' + photos[i]} key={i} />;
+            xSquare3.push(elImg);
+        }
+
+        for (var i = 180 ; i < 196; i++ ){
+            var elImg = <img src={'/photos/' + photos[i]} key={i} />;
+            xSquare4.push(elImg);
+        }
+
+        for (var i = 196 ; i < 212; i++ ){
+            var elImg = <img src={'/photos/' + photos[i]} key={i} />;
+            xSquare5.push(elImg);
+        }
+
+    }
+    fetchData()
+
+    return (
+        <>
+            <P pRoundPart={pRoundPart} pStraightPart={pStraightPart} />
             <div className="i-letter">
-                {/* 36 photos */}
                 <div className="point-part">
-                    { 
-                        photos.map((photo, index) => {
-                            if((index > photosIndex) && (index < 231)) {
-                                photosIndex++;
-                                return <img src={'/photos/' + photo} key={index} />
-                            }
-                            /* At the end photosIndex = 230 */
-                        }) 
+                    {iPointPart.length > 0 &&
+                    iPointPart
                     }
                 </div>
-                {/* 60 photos */}
                 <div className="straight-part">
-                    { 
-                        photos.map((photo, index) => {
-                            if((index > photosIndex) && (index < 291)) {
-                                photosIndex++;
-                                return <img src={'/photos/' + photo} key={index} />
-                            }
-                            /* At the end photosIndex = 290 */
-                        }) 
+                    {iStraightPart.length > 0 &&
+                    iStraightPart
                     }
                 </div>
             </div>
             <div className="x-letter">
                 <div className="col col-1">
-                    {/* 36 photos */}
-                    <div className="square">
-                        { 
-                            photos.map((photo, index) => {
-                                if((index > photosIndex) && (index < 327)) {
-                                    photosIndex++;
-                                    return <img src={'/photos/' + photo} key={index} />
-                                }
-                                /* At the end photosIndex = 326 */
-                            }) 
+                    <div className="square 1">
+                        {xSquare1.length > 0 &&
+                        xSquare1
                         }
                     </div>
                     <div className="square hidden">
                     </div>
-                    {/* 36 photos */}
-                    <div className="square">
-                        { 
-                            photos.map((photo, index) => {
-                                if((index > photosIndex) && (index < 364)) {
-                                    photosIndex++;
-                                    return <img src={'/photos/' + photo} key={index} />
-                                }
-                                /* At the end photosIndex = 363 */
-                            }) 
+                    <div className="square 2">
+                        {xSquare2.length > 0 &&
+                        xSquare2
                         }
                     </div>
                 </div>
                 <div className="col col-2">
                     <div className="square hidden">
-                        
+
                     </div>
-                    {/* 36 photos */}
-                    <div className="square">
-                        { 
-                            photos.map((photo, index) => {
-                                if((index > photosIndex) && (index < 400)) {
-                                    photosIndex++;
-                                    return <img src={'/photos/' + photo} key={index} />
-                                }
-                                /* At the end photosIndex = 399 */
-                            }) 
+                    <div className="square 3">
+                        {xSquare3.length > 0 &&
+                        xSquare3
                         }
                     </div>
                     <div className="square hidden">
@@ -121,31 +140,17 @@ function Logo() {
                     </div>
                 </div>
                 <div className="col col-3">
-                    {/* 36 photos */}
-                    <div className="square">
-                        { 
-                            photos.map((photo, index) => {
-                                if((index > photosIndex) && (index < 436)) {
-                                    photosIndex++;
-                                    return <img src={'/photos/' + photo} key={index} />
-                                }
-                                /* At the end photosIndex = 435 */
-                            }) 
+                    <div className="square 4">
+                        {xSquare4.length > 0 &&
+                        xSquare4
                         }
                     </div>
                     <div className="square hidden">
 
                     </div>
-                    {/* 36 photos */}
-                    <div className="square">
-                        { 
-                            photos.map((photo, index) => {
-                                if((index > photosIndex) && (index < 472)) {
-                                    photosIndex++;
-                                    return <img src={'/photos/' + photo} key={index} />
-                                }
-                                /* At the end photosIndex = 471 */
-                            }) 
+                    <div className="square 5">
+                        {xSquare5.length > 0 &&
+                        xSquare5
                         }
                     </div>
                 </div>
