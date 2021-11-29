a598f071a42776e4bcfefc26a7345a6ec2c4cbf3
Kira <math.descamps5962@gmail.com>
2021-11-29 11:20:56 +0100

    Added border to displayed images.

--- pix/src/components/Logo.js
+++ pix/src/components/Logo.js
@@ -32,7 +32,7 @@ for(var i=0 ; i < 212; i++ ) {
         catch (err) {return 'default.webp'}
     }
     var src = srcPhoto(i);
-    var elm =  <img className={'imgPix'} style={ (src !== 'default.webp')? { opacity: '0'} :{} } src={src} alt={""}  key={i} />
+    var elm =  <div className="img-wrapper" style={ src !== 'default.webp' ? {zIndex: '4', border: '14px solid #12F6EF', boxShadow: 'inset 0px 0px 30px 30px rgba(0,0,0,0.9)', height: '100%', width:'100%'} : {height: '100%', width:'100%'}}><img className={'imgPix'} style={ src !== 'default.webp' ? { opacity: '0', position: 'relative', zIndex: '4'} :{} } src={src} alt={""}  key={i} /></div>
     if(i < 72)                   pix.P.rounded.push(elm);
     else if(i >= 72 && i < 88)   pix.P.straight.push(elm);
     else if(i >= 88 && i < 104)  pix.I.dot.push(elm);
