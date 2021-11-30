49db72b64b1edf40269b15d0375e50fd9603a952
Mathilde <mathilde@macbook-air-de-laurent.home>
2021-11-30 15:38:27 +0100

    SwitchColor animation is running during 12minutes and stops.

--- pix/src/components/Logo.js
+++ pix/src/components/Logo.js
@@ -33,9 +33,9 @@ for(var i=0 ; i < 212; i++ ) {
     }
     var src = srcPhoto(i);
     var elm =  
-        <div style={ {maxWidth: '190px', maxHeight: '190px', overflow: 'hidden', position:'relative'}}>
-            <div className={(src !== 'default.webp') ? 'imgWrapper' : ''} style={ (src !== 'default.webp')? {overflow: 'initial', backgroundColor: 'transparent'/* , border: '14px solid #12F6EF' */} : {} }>
-                <img className={'imgPix'} style={ (src !== 'default.webp')? { opacity: '0', maxWidth: '85%', maxHeight: '85%'} :{} } src={src} alt={""}  key={i} />
+        <div style={ {maxWidth: '190px', maxHeight: '190px', overflow: 'hidden', position:'relative'}} key={i}>
+            <div className={(src !== 'default.webp') ? 'imgWrapper' : ''} style={ (src !== 'default.webp')? {overflow: 'initial', backgroundColor: 'transparent'} : {} }>
+                <img className={'imgPix'} style={ (src !== 'default.webp')? { opacity: '0', maxWidth: '85%', maxHeight: '85%'} :{} } src={src} alt={""} />
             </div>
         </div>
     ;
